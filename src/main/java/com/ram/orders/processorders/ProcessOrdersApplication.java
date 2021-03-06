4fe5707d62010774b163287b6e1994f455966662
rguntuka <guntuka_ramakrishna@optum.com>
2021-03-07 01:36:24 +0530

    First commit;k8s deployment is successful

---
+++ src/main/java/com/ram/orders/processorders/ProcessOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.ram.orders.processorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessOrdersApplication.class, args);
+	}
+
+}
